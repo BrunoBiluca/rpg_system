03c92be02b611fcd700848af1e213abd93a225e9
bruno <bruno.costa@cinnecta.com>
2020-06-08 21:51:18 -0300

    GridMode adição de valores e desenho de linhas

--- Assets/Scripts/GridMode.cs
+++ Assets/Scripts/GridMode.cs
@@ -1,4 +1,8 @@
-﻿using UnityEditor.UI;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEditor.UI;
 using UnityEngine;
 
 public class GridMode {
@@ -7,18 +11,20 @@ public class GridMode {
     private int cellSize;
 
     private int[,] gridArray;
+    private TextMesh[,] gridTextArray;
 
     public GridMode(int width, int height) {
         this.width = width;
         this.height = height;
 
         gridArray = new int[width, height];
+        gridTextArray = new TextMesh[width, height];
 
         cellSize = 4;
 
         for(int x = 0; x < gridArray.GetLength(0); x++) {
             for(int y = 0; y < gridArray.GetLength(1); y++) {
-                CreateWordText(gridArray[x, y].ToString(), GetWorldPosition(x, y));
+                gridTextArray[x, y] = CreateWordText(gridArray[x, y].ToString(), GetWorldPosition(x, y));
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
             }
@@ -31,12 +37,39 @@ public class GridMode {
         return new Vector3(x, y) * cellSize;
     }
 
+    private Vector2 GetGridPostion(Vector3 position) {
+        var gridPosition = new Vector2(
+            (int)Math.Floor(position.x / cellSize),
+            (int)Math.Floor(position.y / cellSize)
+        );
+
+        return gridPosition;
+    }
+
+    private bool IsInsideGrid(int x, int y) {
+        return x >= 0 && x < width
+            && y >= 0 && y < height;
+    }
+
+    private bool IsInsideGrid(Vector3 pos) {
+        return IsInsideGrid((int)pos.x, (int)pos.y);
+    }
+
+    private bool IsInsideGrid(params Vector3[] positions) {
+        foreach(var pos in positions) {
+            if(!IsInsideGrid(pos)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public TextMesh CreateWordText(string text, Vector3 localPosition, Transform parent = null) {
         var gameObject = new GameObject("World_Text", typeof(TextMesh));
 
         var transform = gameObject.transform;
         transform.SetParent(parent, false);
-        transform.localPosition = localPosition + new Vector3(cellSize/2, cellSize/2);
+        transform.localPosition = localPosition + new Vector3(cellSize / 2, cellSize / 2);
 
 
         var textMesh = gameObject.GetComponent<TextMesh>();
@@ -46,5 +79,38 @@ public class GridMode {
         textMesh.anchor = TextAnchor.MiddleCenter;
 
         return textMesh;
+    }
+
+    public bool SetNodeValue(Vector3 position, string value) {
+        var gridPosition = GetGridPostion(position);
+
+        if(!IsInsideGrid((int)gridPosition.x, (int)gridPosition.y))
+            return false;
+
+        gridTextArray[(int)gridPosition.x, (int)gridPosition.y].text = value;
+
+        return true;
+    }
+
+    public bool DrawLines(params Vector3[] positions) {
+
+        var gridPositions = new List<Vector3>();
+        foreach(var pos in positions) {
+            gridPositions.Add(GetGridPostion(pos));
+        }
+
+        if(!IsInsideGrid(gridPositions.ToArray())){
+            return false;
+        }
+
+        for(int i = 1; i < gridPositions.Count; i++) {
+            Debug.DrawLine(
+                GetWorldPosition((int)gridPositions[i - 1].x, (int)gridPositions[i - 1].y) + new Vector3(cellSize / 2 , cellSize / 2, 0), 
+                GetWorldPosition((int)gridPositions[i].x, (int)gridPositions[i].y) + new Vector3(cellSize / 2 , cellSize / 2, 0),
+                Color.white, 
+                100f
+            );
+        }
+        return true;
     }
 }
--- Assets/Scripts/TestGridMode.cs
+++ Assets/Scripts/TestGridMode.cs
@@ -3,7 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TestGridMode : MonoBehaviour {
+
+    GridMode grid;
+
+    List<Vector3> mouseInputs;
+
     void Start() {
-        var grid = new GridMode(5, 5);
+        grid = new GridMode(5, 5);
+
+        mouseInputs = new List<Vector3>();
+    }
+
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            var mouse = Input.mousePosition;
+            var worldPosition = Camera.main.ScreenToWorldPoint(mouse);
+            grid.SetNodeValue(worldPosition, "32");
+
+            mouseInputs.Add(worldPosition);
+            if(mouseInputs.Count >= 2) {
+                var canDraw = grid.DrawLines(mouseInputs.ToArray());
+
+                if(!canDraw) {
+                    mouseInputs.Clear();
+                }
+            }
+                
+            foreach(var input in mouseInputs) {
+                Debug.Log(input);
+            }
+        }
     }
 }
