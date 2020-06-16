b3e06d080abb043bbbc0d33bcc1e3d5c3e6e4477
bruno <bruno.costa@cinnecta.com>
2020-06-15 22:20:09 -0300

    Criação do sistema de batalha

---
+++ Assets/Scripts/Battle/BattleHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleHandler : MonoBehaviour {
+
+    [SerializeField] public GameObject playerPrefab;
+        
+    void Start() {
+        SpawnCharacter(true);
+        SpawnCharacter(false);
+    }
+
+    private void SpawnCharacter(bool isPlayerTeam) {
+        if(isPlayerTeam) {
+            Instantiate(playerPrefab, new Vector3(-1.5f, 0, 0), Quaternion.identity);
+        } else {
+            var gameObject = Instantiate(playerPrefab, new Vector3(1.5f, 0, 0), Quaternion.identity);
+            var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    void Update() {
+
+    }
+}
